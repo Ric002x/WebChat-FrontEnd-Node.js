2f68e7201cc5d9865d846361fe9127f4087a2fae
Ricardo Oliveira <ricvenicius@gmail.com>
2026-01-19 13:50:15 -0300

    refactor: start client project,  and add new theme provider and Toaster component with ShadCn, update package dependencies

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "../components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
+
+const nunito = Nunito({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "WebChat",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br" suppressHydrationWarning>
+      <body
+        className={`${nunito.className}`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+        <Toaster />
+      </body>
+    </html>
+  );
+}
