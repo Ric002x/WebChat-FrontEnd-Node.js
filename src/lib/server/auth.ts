33baa078cc05b125dfb513f03d46b84d9aaa9b09
Ricardo Oliveira <ricvenicius@gmail.com>
2026-01-19 15:46:51 -0300

    feat: add authentication handling and user proxying; implement login, register, and user retrieval functions

---
+++ src/lib/server/auth.ts
@@ -0,0 +1,58 @@
+"use server";
+
+import { login, register } from "@/src/lib/requests";
+import { LoginData, RegisterData } from "@/src/lib/schemas/authSchema";
+import { User } from '@/src/types/User';
+import { cookies } from "next/headers";
+
+export const handleLogin = async (data: LoginData) => {
+    const response = await login(data)
+
+    if (response.data) {
+        (await cookies()).set({
+            name: process.env.NEXT_PUBLIC_AUTH_KEY as string,
+            value: response.data.accessToken,
+            httpOnly: true,
+            maxAge: 86400 * 30
+        })
+    }
+
+    return response
+}
+
+export const handleRegister = async (data: RegisterData) => {
+    const response = await register(data)
+
+    if (response.data) {
+        (await cookies()).set({
+            name: process.env.NEXT_PUBLIC_AUTH_KEY as string,
+            value: response.data.accessToken,
+            httpOnly: true,
+            maxAge: 86400 * 30
+        })
+    }
+
+    return response
+}
+
+export const handleGetUser = async () => {
+    const authCookies = (await cookies()).get(process.env.NEXT_PUBLIC_AUTH_KEY as string)?.value
+
+    const response = await fetch(process.env.NEXT_PUBLIC_API_BASE_URL + '/api/v1/user/me', {
+        headers: {
+            Authorization: `Bearer ${authCookies}`
+        }
+    })
+
+    const jsonResponse = await response.json()
+    const userData = jsonResponse.user
+
+    if (userData) return userData as User
+
+    return null
+}
+
+
+export const handleSignOut = async () => {
+    (await cookies()).delete(process.env.NEXT_PUBLIC_AUTH_KEY as string)
+}
---
+++ src/proxy.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+import { handleGetUser } from "./lib/server/auth";
+
+
+export async function proxy(request: NextRequest) {
+    const user = await handleGetUser()
+
+    // Redirecionar para página de Login caso não houver usuário autenticado
+    if (!request.nextUrl.pathname.startsWith('/auth') && !user) {
+        return NextResponse.redirect(new URL('/auth/login', request.url))
+    }
+
+    if (request.nextUrl.pathname.startsWith('/auth') && user) {
+        return NextResponse.redirect(new URL('/', request.url))
+    }
+}
+
+export const config = {
+    matcher: '/((?!.*\\..*|_next).*)'
+}
